717b6c5cf82d658f53925914701d2beefab0bffe
kimzaaa <kimzamusic69@gmail.com>
2023-09-14 22:06:17 +0700

    School planner programv1.1 banner change and timer

--- main prog.py
+++ main prog.py
@@ -16,6 +16,7 @@ class Window(QMainWindow):
 
     def UiComponents(self):
         fontbold = QFont("Roboto Mono Medium", 15)
+        fontboldsmall = QFont("Roboto Mono Medium", 8)
         font = QFont("Roboto Mono", 15)
         fontthin = QFont("Roboto Mono Thin", 15)
 
@@ -33,15 +34,40 @@ class Window(QMainWindow):
         self.sidetabbar.setGeometry(0, 200, 200, 800)
         self.sidetabbar.setStyleSheet("background-color : #404040 ")
 
-        self.settitlename = QLineEdit(" ", self)
+        self.settitlename = QLineEdit("School work", self)
         self.settitlename.setGeometry(15, 240, 125, 20)
+        self.settitlename.setStyleSheet(
+            "QLineEdit"
+            "{"
+            "background : #545454;"
+            "border : #545454;"
+            "color : white;"
+            "}"
+        )
+        self.settitlename.setFont(fontboldsmall)
 
         self.settitlebtn = QPushButton("✔", self)
         self.settitlebtn.setGeometry(145, 240, 20, 20)
         self.settitlebtn.clicked.connect(self.settitle)
+        self.settitlebtn.setStyleSheet(
+            "QPushButton"
+            "{"
+            "background : #545454;"
+            "border : #545454;"
+            "color : white;"
+            "}"
+        )
         self.unsettitlebtn = QPushButton("X", self)
         self.unsettitlebtn.setGeometry(170, 240, 20, 20)
         self.unsettitlebtn.clicked.connect(self.unsettitle)
+        self.unsettitlebtn.setStyleSheet(
+            "QPushButton"
+            "{"
+            "background : #545454;"
+            "border : #545454;"
+            "color : white;"
+            "}"
+        )
 
         self.main = QLabel(" ", self)
         self.main.setGeometry(200, 200, 1000, 800)
@@ -52,8 +78,21 @@ class Window(QMainWindow):
         self.title.setGeometry(300, 200, 1000, 100)
         self.title.setFont(fontbold)
 
-        self.setGeometry(0, 0, 1200, 800)  # fullscreen
+        self.clock = QLabel("", self)
+        self.clock.setStyleSheet("color : white")
+        self.clock.setGeometry(1030, 200, 1000, 100)
+        self.clock.setFont(fontbold)
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.showTime)
+        self.timer.start(100)
+
+        self.setGeometry(0, 0, 1200, 800)
         self.show()
+
+    def showTime(self):
+        self.current_time = QTime.currentTime()
+        self.label_time = self.current_time.toString("hh:mm:ss")
+        self.clock.setText(self.label_time)
 
     def settitle(self):
         self.title.setText(self.settitlename.text())
